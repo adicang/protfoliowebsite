1ab2f5c2bbae8b9b34eca640af5574c9021d98e3
adicang <adicang@gmail.com>
2019-11-06 15:00:40 +0200

    Updates

---
+++ precache-manifest.c6d3d4458c5733c4e9eb863b9f946421.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc675af246b33f9f5f2ac58dceb13b18",
+    "url": "/protfoliowebsite/index.html"
+  },
+  {
+    "revision": "cb782c6c635a51e8186f",
+    "url": "/protfoliowebsite/static/css/main.3fb1a8ec.chunk.css"
+  },
+  {
+    "revision": "cd51a482d88c51471591",
+    "url": "/protfoliowebsite/static/js/2.6c46d341.chunk.js"
+  },
+  {
+    "revision": "cb782c6c635a51e8186f",
+    "url": "/protfoliowebsite/static/js/main.a0b75c65.chunk.js"
+  },
+  {
+    "revision": "cb191d4825c75066d8cf",
+    "url": "/protfoliowebsite/static/js/runtime-main.1bb44465.js"
+  },
+  {
+    "revision": "e2b1163cefb387257b4ca55b4c1e25a6",
+    "url": "/protfoliowebsite/static/media/backoffice.e2b1163c.gif"
+  },
+  {
+    "revision": "ba6ecd653ea3031395dd7fcb1b624110",
+    "url": "/protfoliowebsite/static/media/bookinGym.ba6ecd65.gif"
+  },
+  {
+    "revision": "450648828f93aa5f736ad929a9b9dd6f",
+    "url": "/protfoliowebsite/static/media/deli.45064882.gif"
+  },
+  {
+    "revision": "6a9c282a71946e10dc842c931aaf2347",
+    "url": "/protfoliowebsite/static/media/mabali.6a9c282a.gif"
+  },
+  {
+    "revision": "a7153a36a454d908d55aad56bb1f5ad6",
+    "url": "/protfoliowebsite/static/media/myImg.a7153a36.jpg"
+  }
+]);
